349f1fc7ec9895efa5825fdcedff0ba1c9bce72d
Ebrahim230 <ebrahimalianik@hotmail.com>
2025-11-17 18:16:03 +0600

    Adding login and signup logics

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,16 +1,14 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const client = new MongoClient(process.env.MONGO_URI);
-
 export const db = client.db("ai_mern");
-
 export const connectDB = async () => {
   try {
     await client.connect();
-    console.log("MongoDB connected");
+    const uri = new URL(process.env.MONGO_URI);
+    const host = uri.hostname;
+    console.log(`Database connected on: ${host}`);
   } catch (err) {
     console.error("MongoDB Error:", err);
     process.exit(1);
